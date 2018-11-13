da8663f8e1e5fd818f68ad34743792a5c2736174
johnnyasd12 <johnnnyasd12@gmail.com>
2018-11-13 20:00:36 +0800

    update teacher architecture

--- models.py
+++ models.py
@@ -13,13 +13,13 @@ def build_big_model(input_dims, output_dims, session):
         , dtype_y=tf.float32)
 
     bigmodel.add_layer(
-        FC(inputs=bigmodel.h[-1],out_dims=64, activation_fn=tf.nn.relu))
-    # bigmodel.add_layer(
-    #     Dropout(inputs=bigmodel.h[-1], keep_prob=0.5))
-    # bigmodel.add_layer(
-    #     FC(inputs=bigmodel.h[-1],out_dims=1200, activation_fn=tf.nn.relu))
-    # bigmodel.add_layer(
-    #     Dropout(inputs=bigmodel.h[-1], keep_prob=0.5))
+        FC(inputs=bigmodel.h[-1],out_dims=512, activation_fn=tf.nn.relu))
+    bigmodel.add_layer(
+        Dropout(inputs=bigmodel.h[-1], keep_prob=0.7))
+    bigmodel.add_layer(
+        FC(inputs=bigmodel.h[-1],out_dims=512, activation_fn=tf.nn.relu))
+    bigmodel.add_layer(
+        Dropout(inputs=bigmodel.h[-1], keep_prob=0.7))
     bigmodel.add_layer(
         FC(inputs=bigmodel.h[-1],out_dims=output_dims, activation_fn=tf.nn.softmax))
 
@@ -40,7 +40,7 @@ def build_small_model(input_dims, output_dims, session, is_student):
         , dtype_y=tf.float32)
 
     smallmodel.add_layer(
-        FC(inputs=smallmodel.h[-1],out_dims=5,activation_fn=tf.nn.relu))
+        FC(inputs=smallmodel.h[-1],out_dims=32,activation_fn=tf.nn.relu))
     smallmodel.add_layer(
         FC(inputs=smallmodel.h[-1],out_dims=output_dims, activation_fn=tf.nn.softmax))
 
@@ -48,7 +48,6 @@ def build_small_model(input_dims, output_dims, session, is_student):
         smallmodel.compile_student(
             loss_standard=tf.losses.softmax_cross_entropy(smallmodel.ys,smallmodel.logits)
             , opt=tf.train.AdamOptimizer(learning_rate=1e-3) #TODO: higher learning rate
-            # , opt=tf.train.AdagradOptimizer(learning_rate=1e-2)
             , metrics = ['acc']) # BUGFIX: logits_with_T still None currently
     else:
         smallmodel.compile_nn(
